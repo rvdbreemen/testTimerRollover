c7d660523d614899be40439faaf72a5c73d88d72
mrWheel <5585427+mrWheel@users.noreply.github.com>
2020-01-31 16:38:05 +0100

    Update timers.h

--- testTimerRollover/timers.h
+++ testTimerRollover/timers.h
@@ -43,6 +43,6 @@
 //--- to test the roll-over in a reasanable time frame I defined the same functions ---
 //--- but with a 8 bit unsigned integer                                             ---
 //-------------------------------------------------------------------------------------
-#define DECLARE_LOWTIMER(timerName, timerTime)    static uint8_t timerName##_interval = timerTime,      timerName##_last = myTimer+random(timerName##_interval);
-#define SINCE_LOW(timerName)  ((int8_t)(myTimer - timerName##_last))
-#define DUE_LOW(timerName) ((SINCE_LOW(timerName) < timerName##_interval) ? 0 : (timerName##_last=myTimer))
+#define DECLARE_8BITTIMER(timerName, timerTime)    static uint8_t timerName##_interval = timerTime,      timerName##_last = myTimer+random(timerName##_interval);
+#define SINCE_8BIT(timerName)                      ((int8_t)(myTimer - timerName##_last))
+#define DUE_8BIT(timerName)                        ((SINCE_8BIT(timerName) < timerName##_interval) ? 0 : (timerName##_last=myTimer))
