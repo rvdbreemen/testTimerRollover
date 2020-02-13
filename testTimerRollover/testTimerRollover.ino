f9e1cb5738340f8e62b17525d81e7804c34ea666
mrWheel <5585427+mrWheel@users.noreply.github.com>
2020-02-13 10:14:16 +0100

    Update testTimerRollover.ino

--- testTimerRollover/testTimerRollover.ino
+++ testTimerRollover/testTimerRollover.ino
@@ -6,20 +6,22 @@
  * I have made some _LOW macro's that are used with a semi millis()
  * Function wordt uint8_t (8 bit) timers so you don't have to wait
  * 49 day's before a RollOver occurs.
- * Just remove the _LOW macro's if you want to use the timers.h file
+ * Just remove the 8Bit macro's if you want to use the safeTimers.h file
  * in your program.
  */
 
-static uint8_t myTimer = 0;
+#include "safeTimers.h"
 
-#include "timers.h"
-
-  DECLARE_TIMERms(waiting, 50)          // update myTimer every 50 ms
-  DECLARE_8BIT_TIMER(printTheText, 15)  // print text every 15 myTimer's
+  DECLARE_TIMERms(every100ms, 100)      // update myTimer every 100 ms
+  DECLARE_8BIT_TIMER(test8Bit, 15)  // print text every 15 timer8Bit() something
 
 void printText()
 {
-  Serial.printf("myTimer[%6u], x_interval[%6u] x_last[%6u]  ..\r\n", myTimer, printTheText_interval, printTheText_last);
+  Serial.printf("[%02d:%02d:%03d] timer8Bit()[%6u], x_last[%6u]  ..\r\n", ((millis()/ (60 * 1000)) % 60)
+                                                                        , ((millis() / 1000) % 60)
+                                                                        , (millis() % 1000)
+                                                                        , timer8Bit()
+                                                                        , test8Bit_last);
 }
 
 void setup() {
@@ -29,12 +31,12 @@ void setup() {
 }
 
 void loop() {
-  if ( DUE(waiting))
+  if ( DUE(every100ms))
   {
     myTimer++;
     Serial.print(".");
   }      
-  if ( DUE_8BIT(printTheText) ) {
+  if ( DUE_8BIT(test8Bit) ) {
       printText();
   }
 
