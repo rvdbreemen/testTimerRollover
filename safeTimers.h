8be9bb71a9470681b7a274dc6362e0be7401412d
mrWheel <5585427+mrWheel@users.noreply.github.com>
2020-02-13 13:34:38 +0100

    more explaining example

--- safeTimers.h
+++ safeTimers.h
@@ -1,5 +1,5 @@
 /*
- * timers.h is developed by Erik
+ * safeTimers.h is developed by Erik
  * 
  * I made some small changes due to the "how can I handle the millis() rollover"
  * by Edgar Bonet
@@ -17,9 +17,19 @@
  *          true (current millis) if it has
  *  updates <timername>_last
  *  
+ *  RESTART_TIMER(timername)
+ *   it restarts the timer
+ *   updates <timername>_last
+ *  
  *  Usage example:
  *  
  *  DECLARE_TIMER(screenUpdate, 200) // update screen every 200 ms
+ *  ...
+ *  setup()
+ *  {
+ *  ..
+ *    RESTART_TIMER(screenUpdate);  // restart the timer for 200 ms!
+ *  }
  *  ...
  *  loop()
  *  {
@@ -30,12 +40,20 @@
  *  }
  *  
  *  https://arduino.stackexchange.com/questions/12587/how-can-i-handle-the-millis-rollover
+ *  
+ *  Be awair in some rare situations the timer's take twice as long as antisipated!
+ *  
  */
-#define DECLARE_TIMERm(timerName, timerTime)    static unsigned long timerName##_interval = timerTime * 60 * 1000,      timerName##_last = millis()+random(timerName##_interval);
-#define DECLARE_TIMER(timerName, timerTime)     static unsigned long timerName##_interval = timerTime * 1000,           timerName##_last = millis()+random(timerName##_interval);
-#define DECLARE_TIMERms(timerName, timerTime)   static unsigned long timerName##_interval = timerTime,                  timerName##_last = millis()+random(timerName##_interval);
+#define DECLARE_TIMERm(timerName, timerTime)    static unsigned long timerName##_interval = timerTime * 60 * 1000,     \
+                                                timerName##_last = millis();
+#define DECLARE_TIMER(timerName, timerTime)     static unsigned long timerName##_interval = timerTime * 1000,          \
+                                                timerName##_last = millis();
+#define DECLARE_TIMERms(timerName, timerTime)   static unsigned long timerName##_interval = timerTime,                 \
+                                                timerName##_last = millis();
 
 #define DECLARE_TIMERs DECLARE_TIMER
+
+#define RESTART_TIMER(timerName)                { timerName##_last = millis(); }
 
 #define SINCE(timerName)  ((signed long)(millis() - timerName##_last))
 #define DUE(timerName) (( SINCE(timerName) < timerName##_interval) ? 0 : (timerName##_last=millis()))
@@ -43,16 +61,27 @@
 //-------------------------------------------------------------------------------------
 //--- to test the roll-over in a reasanable time frame I defined the same functions ---
 //--- but with a 8 bit unsigned integer                                             ---
+//--- Rollover takes place every 25 seconds                                         ---
 //-------------------------------------------------------------------------------------
-static uint8 myTimer;
+uint8_t myTimer = 0;
 
 uint8_t timer8Bit()
 {
   return myTimer;
+  
 } // timer8Bit()
 
-#define DECLARE_8BIT_TIMER(timerName, timerTime)    static uint8_t timerName##_interval = timerTime,     \
-                                                    timerName##_last = timer8Bit()+random(timerName##_interval);
+
+#define DECLARE_8BIT_TIMER(timerName, timerTime)    static uint8_t timerName##_interval = timerTime,  \
+                                                    timerName##_last = timer8Bit();
+
+#define RESTART_8BIT_TIMER(timerName)               { timerName##_last = timer8Bit(); }
+
 #define SINCE_8BIT(timerName)                       ((int8_t)(timer8Bit() - timerName##_last))
-#define DUE_8BIT(timerName)                         ((SINCE_8BIT(timerName) < timerName##_interval) ?    \
-                                                                          0 : (timerName##_last=timer8Bit()))
+#define DUE_8BIT(timerName)                         ((SINCE_8BIT(timerName) < timerName##_interval)   \
+                                                        ? 0 : (timerName##_last=timer8Bit()))
+
+/* 
+**      
+** eof 
+*/ 
--- testTimerRollover.ino
+++ testTimerRollover.ino
@@ -3,41 +3,95 @@
  * Thanks to Erik and Edgar Bonet
  * 
  * A program to test the millis() RollOver debacle
- * I have made some _LOW macro's that are used with a semi millis()
- * Function wordt uint8_t (8 bit) timers so you don't have to wait
- * 49 day's before a RollOver occurs.
+ * I have made some 8bit macro's that are used with a semi millis()
+ * (8 bit) timers so you don't have to wait 49 day's before a RollOver 
+ * occurs.
  * Just remove the 8Bit macro's if you want to use the safeTimers.h file
  * in your program.
  */
 
+#define MS_TICKS      100       // update timer8Bit() every 100ms
+#define TEST_INTERVAL  30       // set 8bit timer to nr of ticks
+
 #include "safeTimers.h"
 
-  DECLARE_TIMERms(every100ms, 100)      // update myTimer every 100 ms
-  DECLARE_8BIT_TIMER(test8Bit, 15)  // print text every 15 timer8Bit() something
+  DECLARE_TIMERms(everyMsTick, MS_TICKS)        // update myTimer every msTick
+  DECLARE_8BIT_TIMER(test8Bit, TEST_INTERVAL)   // print text every INTERVAL timer8Bit() 
 
-void printText()
+  DECLARE_TIMERs(wait3Sec, 3)                   // delay 3 seconds
+  DECLARE_TIMERs(doDelay, 10)                   // every 10 seconds 
+
+//================================================================================================
+void print8BitText()
 {
-  Serial.printf("[%02d:%02d:%03d] timer8Bit()[%6u], x_last[%6u]  ..\r\n", ((millis()/ (60 * 1000)) % 60)
-                                                                        , ((millis() / 1000) % 60)
-                                                                        , (millis() % 1000)
-                                                                        , timer8Bit()
-                                                                        , test8Bit_last);
-}
+  static uint32_t lastPrint = millis();
+  int duration = millis() - lastPrint;
+  
+  if (duration > (TEST_INTERVAL * MS_TICKS))
+  {
+    Serial.printf("\r\n[%4d]ticks [%02d:%02d:%03d] timer8Bit()[%4u]/[%4d] => DUE_8BIT() TOOK TO LONG!!!\r\n"
+                                                           , ((millis() - lastPrint) / MS_TICKS)
+                                                           , ((millis() / (60 * 1000)) % 60)
+                                                           , ((millis() / 1000) % 60)
+                                                           , (millis() % 1000)
+                                                           , timer8Bit()
+                                                           , (int8_t)timer8Bit());
+  }
+  else
+  {
+    Serial.printf("[%4d]ticks [%02d:%02d:%03d]  DUE_8BIT() fired!\r\n"
+                                                           , ((millis() - lastPrint) / MS_TICKS)
+                                                           , ((millis() / (60 * 1000)) % 60)
+                                                           , ((millis() / 1000) % 60)
+                                                           , (millis() % 1000));
+    
+  }
+  lastPrint = millis();
+  
+} // print8BitText()
 
-void setup() {
-   Serial.begin(115200);
-   Serial.println("\n.. and then it begins ...\n");
 
-}
-
-void loop() {
-  if ( DUE(every100ms))
+//================================================================================================
+void update8BitTimer(char x)
+{
+  if ( DUE(everyMsTick))
   {
     myTimer++;
-    Serial.print(".");
+    Serial.print(x);
   }      
+  yield();
+  
+} // update8BitTimer()
+
+
+//================================================================================================
+void setup() {
+  Serial.begin(115200);
+  Serial.println("\n.. and then it begins ...\r\n");
+  Serial.printf("\nInterval [%d]ticks\r\n", TEST_INTERVAL);
+
+  RESTART_8BIT_TIMER(test8Bit);
+  
+} // setup()
+
+
+//================================================================================================
+void loop() {
+  update8BitTimer('.');   
+  
   if ( DUE_8BIT(test8Bit) ) {
-      printText();
+      print8BitText();
   }
 
-}
+  if ( DUE(doDelay) )
+  {
+    Serial.print("wait");
+    RESTART_TIMER(wait3Sec);
+    while ( !DUE(wait3Sec) )
+    {
+      update8BitTimer('_');
+    }
+    RESTART_TIMER(doDelay);
+  }
+
+} // loop()
